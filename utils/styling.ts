c5a717a029fedcaf466ab15b886b9e500064134a
samsaem <samhpham@ucla.edu>
2025-03-28 13:04:21 -0700

    set up

---
+++ utils/styling.ts
@@ -0,0 +1,20 @@
+// npm react-native-size-matters
+
+import { Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+const [shortDimension, longDimension] = width < height ? [width, height] : [height, width];
+
+//Default guideline sizes are based on standard ~5" screen mobile device
+const guidelineBaseWidth = 375;
+const guidelineBaseHeight = 812;
+
+export const scale = size => shortDimension / guidelineBaseWidth * size;
+export const verticalScale = size => longDimension / guidelineBaseHeight * size;
+export const moderateScale = (size, factor = 0.5) => size + (scale(size) - size) * factor;
+export const moderateVerticalScale = (size, factor = 0.5) => size + (verticalScale(size) - size) * factor;
+
+export const s = scale;
+export const vs = verticalScale;
+export const ms = moderateScale;
+export const mvs = moderateVerticalScale;
