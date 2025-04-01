ec1ddd626541835cff649336253bd7f09967b3c8
samsaem <samhpham@ucla.edu>
2025-04-01 10:16:49 -0700

    UI for bottom tabs navigator

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,61 @@
-import {StyleSheet, Text, View} from 'react-native'
+import {StyleSheet} from 'react-native'
 import React from 'react'
 import {Tabs} from "expo-router";
-import {CustomTabs} from "@/components/CustomTabs";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const _Layout = () => {
     return (
         <Tabs
-            tabBar={CustomTabs}
-            screenOptions={{headerShown: false}}>
+            screenOptions={{
+                headerShown: false,
+                headerTitleStyle: {
+                    fontWeight: "600",
+                },
+                headerShadowVisible: false,
+                tabBarStyle: {
+                    borderTopWidth: 1,
+                    paddingTop: 4,
+                    //paddingBottom: 2,
+                    //height: 60,
+                }
+            }}
+        >
             <Tabs.Screen
                 name="index"
                 options={{
-                    title:'Home'
+                    title:'Home',
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home-outline" size={26} color="black" />
+                    )
                 }}
             />
             <Tabs.Screen
                 name="summary"
                 options={{
-                    title:'Summary'
+                    title:'Summary',
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="list-outline" size={26} color="black" />
+                    )
                 }}
             />
             <Tabs.Screen
                 name="profile"
                 options={{
-                    title:'Profile'
+                    title:'Profile',
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="person-outline" size={26} color="black" />
+                    )
                 }}
+
             />
         </Tabs>
     )
 }
 export default _Layout
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})
