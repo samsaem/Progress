cdfc051ad2b0f3cd01c5060754aa57379ea29119
samsaem <samhpham@ucla.edu>
2025-04-02 19:14:15 -0700

    update welcome page

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,19 +1,90 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native'
-import React from 'react'
-import {useRouter} from "expo-router";
-import LogIn from "@/app/(auth)/login";
+import { View, StyleSheet, TouchableOpacity, Image, Text } from "react-native";
+import React from "react";
+import {router, useRouter} from "expo-router";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import { scale, verticalScale } from "@/utils/styling";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
+import Animated, { FadeIn, FadeInDown } from "react-native-reanimated";
 
-const Welcome = () => {
+const WelcomePage = () => {
     const router = useRouter();
     return (
-        <LogIn/>
-    )
-}
-export default Welcome
+        <ScreenWrapper>
+            <View style={styles.container}>
+                <View>
+                    <Animated.Image
+                        entering={FadeIn.duration(500)}
+                        source={require("../../assets/images/nyancat.gif")}
+                        style={styles.welcomeTopImage}
+                        resizeMode="contain"
+                    />
+
+                    <Text style={{
+                        fontSize: 22,
+                        fontWeight: 700,
+                        alignSelf: "center",
+                    }}>Welcome to Progress 🎉</Text>
+
+                    <Animated.Image
+                        entering={FadeIn.duration(500)}
+                        source={require("../../assets/images/nyancat.gif")}
+                        style={styles.welcomeTopImage}
+                        resizeMode="contain"
+                    />
+
+                    <View>
+                        <Animated.View
+                            entering={FadeInDown.duration(500).springify().damping(12)}
+                            style={{ alignItems: "center"
+                        }}
+                        >
+
+                            <View style={styles.loginContainer}>
+                                <TouchableOpacity
+                                    onPress={() => router.push('/(auth)/login')}
+                                    style={styles.buttonContainer}
+                                >
+                                    <Text style={styles.loginButton}>Let's get started!</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </Animated.View>
+                    </View>
+                </View>
+            </View>
+        </ScreenWrapper>
+    );
+};
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: "space-between",
+        gap: spacingY._20,
     },
-})
+    welcomeTopImage: {
+        width: "100%",
+        height: verticalScale(300),
+        alignSelf: "center",
+    },
+    welcomeBottomImage: {
+        width: "100%",
+        height: verticalScale(300),
+        alignSelf: "flex-end",
+    },
+    loginButton: {
+        alignSelf: "center",
+        fontWeight: "600",
+        color: 'white',
+        fontSize: 19,
+    },
+    buttonContainer: {
+        width: "50%",
+        padding: 12,
+        backgroundColor: '#067FD0',
+        borderRadius: 12,
+    },
+    loginContainer: {
+    }
+});
+
+export default WelcomePage;
